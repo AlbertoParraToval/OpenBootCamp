2ca9e879033da29ff622d20ea2cd785dd94b99c1
AlbertoParraToval <alberto.parra.fp@iescampanillas.com>
2022-09-29 19:29:27 +0200

    act

---
+++ mi-primer-proyecto-de-node/notacion.js
@@ -0,0 +1,28 @@
+//Notación 
+
+//; -> Sirve para delimitar el final de una linea
+const b = "Hola";
+b + 4;
+//. -> Se utiliza en los objetos para acceder a los atributos
+// movil.anchura
+
+//[] -> Se utiliza para las listas, arreglos o arrays.
+const ar =[1,2,3,4]
+console.log(ar[2]);
+
+//() -> Funciones
+function suma(a,b) {
+  //Se escribe la funcion
+}
+
+//{} -> Llaves para objetos, funciones y estructuras de control.
+const movil = {
+  anchura :5,
+  altura:10
+}
+
+if (true) {
+  //todo lo que haya entra llaves
+
+  const z = "hola caracola";
+}
