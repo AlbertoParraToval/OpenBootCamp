ec7db16aebee17860a67a0d75b9df9da3b77c0ec
AlbertoParraToval <alberto.parra.fp@iescampanillas.com>
2022-09-29 19:21:49 +0200

    Actualizando

--- mi-primer-proyecto-de-node/index.js
+++ mi-primer-proyecto-de-node/index.js
@@ -3,3 +3,6 @@ console.log("Esta es la puerta de entrada al proyecto");
 
 //Este es el mensaje que se envia de despedida
 console.log("Adiós");
+
+
+
---
+++ mi-primer-proyecto-de-node/tipos_primitivos.js
@@ -0,0 +1,25 @@
+/*Tipos Primitivos*/
+
+//Number
+4
+
+//Strings
+"Hola mundo"
+'Hola mundo'
+`Hola mundo`
+
+//booleanos
+true;
+false;
+
+//nulo y undefined
+null;
+undefined; 
+
+//null, undefined, false, 0
+
+if (null) {
+  console.log("cumple")
+} else { 
+  console.log("No cumple")
+}
